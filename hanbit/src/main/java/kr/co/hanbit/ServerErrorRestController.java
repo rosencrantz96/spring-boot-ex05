b01acb47a281a9f81dae78a7f8b2ac01a5d8dc3a
권수경 <topgdvidsyb@naver.com>
2024-06-03 10:26:23 +0900

    HTTP 통신 실습 코드

---
+++ hanbit/src/main/java/kr/co/hanbit/ServerErrorRestController.java
@@ -0,0 +1,14 @@
+package kr.co.hanbit;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerErrorRestController {
+
+    @RequestMapping("/throwServerError")
+    public void throwServerError() {
+        throw new RuntimeException();
+    }
+
+}
